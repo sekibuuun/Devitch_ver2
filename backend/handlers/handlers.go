424c9dc82d9146103f7fb405c40c49eeba7bb60e
sekibuuun <akaunto.no.001@gmail.com>
2024-08-13 13:41:24 +0900

    feat: hellohandlerの作成

---
+++ backend/handlers/handlers.go
@@ -0,0 +1,12 @@
+package handlers
+
+import (
+	"net/http"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Write([]byte("Hello! This is a test server."))
+}
