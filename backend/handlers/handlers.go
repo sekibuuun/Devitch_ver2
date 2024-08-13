e4b5582d6fbfe9365637385f1ae684d76f1ee8b6
sekibuuun <akaunto.no.001@gmail.com>
2024-08-13 05:19:16 +0000

    fix: jsonにする

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -1,12 +1,32 @@
 package handlers
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/sekibuuun/Devitch_ver2/backend/models"
 )
+
+var hellos = []models.Hello{
+	{ID: 1, Content: "Hello!"},
+}
 
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-	w.Write([]byte("Hello! This is a test server."))
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&hellos); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
+
+	_, err := fmt.Fprint(w, buf.String())
+	if err != nil {
+		return
+	}
 }
