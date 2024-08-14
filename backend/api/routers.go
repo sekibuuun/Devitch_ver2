cb2915f0e1db397e785f9b8860e46415dfe94182
sekibuuun <akaunto.no.001@gmail.com>
2024-08-14 10:19:05 +0000

    feat: route追加

--- backend/api/routers.go
+++ backend/api/routers.go
@@ -10,5 +10,6 @@ import (
 func NewRouter() *mux.Router {
 	r := mux.NewRouter()
 	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/genres", handlers.GenresHandler).Methods(http.MethodGet)
 	return r
 }
