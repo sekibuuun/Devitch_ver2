89f2976aab35df6cd577146510632bdce022cece
sekibuuun <akaunto.no.001@gmail.com>
2024-08-14 10:19:27 +0000

    fix: GenreHandler作成

--- backend/handlers/handlers.go
+++ backend/handlers/handlers.go
@@ -8,16 +8,21 @@ import (
 	"net/http"
 
 	"github.com/sekibuuun/Devitch_ver2/backend/models"
+	"github.com/sekibuuun/Devitch_ver2/backend/services"
 )
 
 var hellos = []models.Hello{
 	{ID: 1, Content: "Hello!"},
 }
 
-func HelloHandler(w http.ResponseWriter, r *http.Request) {
+func CORS(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	CORS(w)
 	var buf bytes.Buffer
 	enc := json.NewEncoder(&buf)
 	if err := enc.Encode(&hellos); err != nil {
@@ -26,6 +31,26 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(buf.String())
 
 	_, err := fmt.Fprint(w, buf.String())
+	if err != nil {
+		return
+	}
+}
+
+func GenresHandler(w http.ResponseWriter, r *http.Request) {
+	CORS(w)
+	genres, err := services.GetGenreService()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&genres); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(buf.String())
+
+	_, err = fmt.Fprint(w, buf.String())
 	if err != nil {
 		return
 	}
