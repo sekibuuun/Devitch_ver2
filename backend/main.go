ae22a6ec13945be6eb14191ea43c9b54d8e31b36
sekibuuun <akaunto.no.001@gmail.com>
2024-08-14 10:14:56 +0000

    ref

--- backend/main.go
+++ backend/main.go
@@ -1,36 +1,14 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/sekibuuun/Devitch_ver2/backend/api"
 )
 
-var (
-	dbUser     = os.Getenv("MYSQL_ROOT_USER")
-	dbPassword = os.Getenv("MYSQL_ROOT_PASSWORD")
-	dbDatabase = os.Getenv("MYSQL_DATABASE")
-	dbHost     = os.Getenv("MYSQL_HOST")
-	dbConn     = fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbDatabase)
-)
-
 func main() {
-	db, err := sql.Open("mysql", dbConn)
-	if err != nil {
-		log.Fatal("fail to connect db")
-		return
-	}
-
-	if err := db.Ping(); err != nil {
-		fmt.Printf("failed to connect to DB %v", err)
-	} else {
-		fmt.Println("connect to DB")
-	}
 	// ルーターの初期化
 	r := api.NewRouter()
 
