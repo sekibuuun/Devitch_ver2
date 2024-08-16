a82df680a42cbbd6a5564fcd9c54ee95b3c9256f
sekibuuun <akaunto.no.001@gmail.com>
2024-08-16 13:02:47 +0000

    feat: モデル追加

--- backend/models/models.go
+++ backend/models/models.go
@@ -5,7 +5,41 @@ type Hello struct {
 	Content string `json:"content"`
 }
 
-type Genres struct {
+type Genre struct {
 	GenreId int    `json:"genre_id"`
 	Genre   string `json:"genre"`
+}
+
+type Stream struct {
+	StreamId        int       `json:"stream_id"`
+	Title           string    `json:"title"`
+	GenreIds        []int     `json:"genre_ids"`
+	Comments        []Comment `json:"comments"`
+	CommentsLength  int       `json:"comment_length"`
+	Listeners       []User    `json:"listeners"`
+	ListenersLength int       `json:"listener_length"`
+}
+
+type Comment struct {
+	CommentId int    `json:"comment_id"`
+	StreamId  int    `json:"stream_id"`
+	UserId    int    `json:"user_id"`
+	Content   string `json:"content"`
+	SendAt    string `json:"send_at"`
+}
+
+type User struct {
+	UserID   int      `json:"user_id"`
+	UserName string   `json:"user_name"`
+	Links    []string `json:"links"`
+}
+
+type StreamGenre struct {
+	StreamId int   `json:"stream_id"`
+	GenreId  []int `json:"genre_id"`
+}
+
+type StreamRequest struct {
+	Title    string `json:"title"`
+	GenreIds []int  `json:"genre_ids"`
 }
