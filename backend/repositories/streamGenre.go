8b3d697d5f686122eb0c9b9373e93f7b20ecb249
sekibuuun <akaunto.no.001@gmail.com>
2024-08-18 07:22:35 +0000

    fix: bulk insertを使用

--- backend/repositories/streamGenre.go
+++ backend/repositories/streamGenre.go
@@ -2,19 +2,26 @@ package repositories
 
 import (
 	"database/sql"
+	"strings"
 )
 
 func InsertStreamGenre(db *sql.DB, stream_id int, genre_ids []int) ([]int, error) {
-	const query = `INSERT INTO StreamGenre (stream_id, genre_id) VALUES (?, ?)`
+	const query = `INSERT INTO StreamGenre (stream_id, genre_id) VALUES `
 
-	genres := make([]int, 0, len(genre_ids))
+	placeholders := make([]string, len(genre_ids))
+	args := make([]interface{}, 0, len(genre_ids)*2)
 
-	for _, genre_id := range genre_ids {
-		if _, err := db.Exec(query, stream_id, genre_id); err != nil {
-			return nil, err
-		}
-		genres = append(genres, genre_id)
+	for i, genre_id := range genre_ids {
+		placeholders[i] = "(?, ?)"
+		args = append(args, stream_id, genre_id)
 	}
 
-	return genres, nil
+	fullQuery := query + strings.Join(placeholders, ", ")
+
+	_, err := db.Exec(fullQuery, args...)
+	if err != nil {
+		return nil, err
+	}
+
+	return genre_ids, nil
 }
