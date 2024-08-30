75c1358d8d27654c581d77abc8d3a0df387a9af1
sekibuuun <akaunto.no.001@gmail.com>
2024-08-30 12:34:00 +0000

    feat: コントローラー層の使用

--- backend/api/routers.go
+++ backend/api/routers.go
@@ -1,18 +1,24 @@
 package api
 
 import (
+	"database/sql"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/sekibuuun/Devitch_ver2/backend/api/middlewares"
-	"github.com/sekibuuun/Devitch_ver2/backend/handlers"
+	"github.com/sekibuuun/Devitch_ver2/backend/controllers"
+	"github.com/sekibuuun/Devitch_ver2/backend/services"
 )
 
-func NewRouter() *mux.Router {
+func NewRouter(db *sql.DB) *mux.Router {
+	ser := services.NewMyAppService(db)
+	gCon := controllers.NewGenreController(ser)
+	sCon := controllers.NewStreamController(ser)
+
 	r := mux.NewRouter()
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
-	r.HandleFunc("/genres", handlers.GenresHandler).Methods(http.MethodGet)
-	r.HandleFunc("/streams", handlers.PostStreamHandler).Methods(http.MethodPost)
+	r.HandleFunc("/hello", gCon.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/genres", gCon.GenresHandler).Methods(http.MethodGet)
+	r.HandleFunc("/streams", sCon.PostStreamHandler).Methods(http.MethodPost)
 
 	r.Use(middlewares.CORS)
 	r.Use(middlewares.JSON)
