49b89b0eac789d35cb33f3b7126d92a4653542ca
sekibuuun <akaunto.no.001@gmail.com>
2024-10-01 16:50:58 +0900

    ref

--- backend/controllers/genre_controller.go
+++ backend/controllers/genre_controller.go
@@ -6,14 +6,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
+	"github.com/gorilla/mux"
 	"github.com/sekibuuun/Devitch_ver2/backend/controllers/services"
-	"github.com/sekibuuun/Devitch_ver2/backend/models"
 )
-
-var hellos = []models.Hello{
-	{ID: 1, Content: "Hello!"},
-}
 
 type GenreController struct {
 	service services.GenreServicer
@@ -21,20 +18,6 @@ type GenreController struct {
 
 func NewGenreController(s services.GenreServicer) *GenreController {
 	return &GenreController{service: s}
-}
-
-func (c *GenreController) HelloHandler(w http.ResponseWriter, r *http.Request) {
-	var buf bytes.Buffer
-	enc := json.NewEncoder(&buf)
-	if err := enc.Encode(&hellos); err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(buf.String())
-
-	_, err := fmt.Fprint(w, buf.String())
-	if err != nil {
-		return
-	}
 }
 
 func (c *GenreController) GenresHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,6 +32,41 @@ func (c *GenreController) GenresHandler(w http.ResponseWriter, r *http.Request) 
 	enc := json.NewEncoder(&buf)
 	if err := enc.Encode(&genres); err != nil {
 		log.Println("Could not encode genres to JSON")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(buf.String())
+
+	_, err = fmt.Fprint(w, buf.String())
+	if err != nil {
+		log.Println("Error writing response:", err)
+	}
+}
+
+func (c *GenreController) GetGenreHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	genreID := vars["genre_id"]
+
+	log.Println("genre_id:", genreID)
+
+	id, err := strconv.Atoi(genreID)
+	if err != nil {
+		log.Println("Invalid genre ID")
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+		return
+	}
+
+	genre, err := c.service.GetGenreByIDService(id)
+	if err != nil {
+		log.Println("Could not get genre")
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	var buf bytes.Buffer
+	enc := json.NewEncoder(&buf)
+	if err := enc.Encode(&genre); err != nil {
+		log.Println("Could not encode genre to JSON")
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
