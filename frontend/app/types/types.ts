8a31106d225072dace31b2a9dacbe0999b3108a3
sekibuuun <akaunto.no.001@gmail.com>
2024-10-04 19:36:43 +0900

    型定義

--- frontend/app/types/types.ts
+++ frontend/app/types/types.ts
@@ -7,3 +7,27 @@ export type Genre = {
 	genre_id: number;
 	genre: string;
 };
+
+export type Stream = {
+	stream_id: number;
+	title: string;
+	genre_ids: number[];
+	comment_length: number;
+	comments: Comment[];
+	listener_length: number;
+	listeners: User[];
+};
+
+export type Comment = {
+	comment_id: number;
+	stream_id: number;
+	user_id: number;
+	content: string;
+	send_at: string;
+};
+
+export type User = {
+	user_id: number;
+	user_name: string;
+	links: string[];
+};
